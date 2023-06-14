6d4beff1f9b55697ae8412ec0b95466c5058d144
MAraya <marioaraya1999@outlook.com>
2023-06-13 22:29:51 -0600

    eliminar escrituras que no se usan

--- P3_Mojo/images/imagenPython.py
+++ P3_Mojo/images/imagenPython.py
@@ -15,7 +15,7 @@ def aplicar_filtro(imagen):
     return img_filtrada
 
 # Ruta de la imagen
-ruta_imagen = "images/perros.jpg"
+ruta_imagen = "/home/mario/Desktop/framirez_computer_architecture_2_2023/P3_Mojo/images/perros.jpg"
 
 # Tiempo inicial de ejecucion
 inicio = time.time()
@@ -29,18 +29,3 @@ print("Tiempo de ejecución:", tiempo_ejecucion, "segundos")
 
 # Mostrar imagen con el filtro aplicado
 img_resultante.show()
-
-# Agregar el tiempo de ejecucion en el archivo de texto
-nombre_archivo = "images/tiempos_ejecucion.txt"
-contenido_existente = {}
-try:
-    with open(nombre_archivo, "r") as archivo:
-        contenido_existente = eval(archivo.read())
-except FileNotFoundError:
-    pass
-
-contenido_existente["TiempoPython"] = tiempo_ejecucion
-
-# Escribir el contenido actualizado en el archivo
-with open(nombre_archivo, "w") as archivo:
-    archivo.write(str(contenido_existente))
